6ddfcc4b0288f79f2c980579a49e0529688e7ff7
kannan <karoumont.travail@gmail.com>
2020-09-16 16:20:56 +0200

    r2d2 example: updated main.rs to create table + testing passing a sync routine to route handler.

--- r2d2/src/main.rs
+++ r2d2/src/main.rs
@@ -5,7 +5,7 @@ use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 
-/// Async request handler. Ddb pool is stored in application state.
+/// Async request handler. Db pool is stored in application state.
 async fn index(
     path: web::Path<String>,
     db: web::Data<Pool<SqliteConnectionManager>>,
@@ -14,12 +14,31 @@ async fn index(
     let res = web::block(move || {
         let conn = db.get().unwrap();
 
-        let uuid = format!("{}", uuid::Uuid::new_v4());
+        // create table users if needed
         conn.execute(
-            "INSERT INTO users (id, name) VALUES ($1, $2)",
-            &[&uuid, &path.into_inner()],
+            "CREATE TABLE IF NOT EXISTS users (
+                        id TEXT PRIMARY KEY, 
+                        name TEXT NOT NULL
+            )",
+            rusqlite::params![],
         )
         .unwrap();
+
+        let uuid = format!("{}", uuid::Uuid::new_v4());
+        let name = path.into_inner();
+
+        // println!("{} {}", uuid, name);
+
+        conn.execute(
+            "INSERT INTO users (id, name) VALUES ($1, $2)",
+            rusqlite::params![&uuid, &name],
+        )
+        .unwrap();
+        /*
+        {
+            Ok(_) => println!("Insert ok"),
+            Err(e) => println!("{:?}", e),
+        } */
 
         conn.query_row("SELECT name FROM users WHERE id=$1", &[&uuid], |row| {
             row.get::<_, String>(0)
@@ -28,6 +47,43 @@ async fn index(
     .await
     .map(|user| HttpResponse::Ok().json(user))
     .map_err(|_| HttpResponse::InternalServerError())?;
+    Ok(res)
+}
+
+/// Sync request handler. Db pool is stored in application state.
+async fn index2(
+    path: web::Path<String>,
+    db: web::Data<Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, Error> {
+    let conn = db.get().unwrap();
+
+    // create table users if needed
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+                        id TEXT PRIMARY KEY, 
+                        name TEXT NOT NULL
+            )",
+        rusqlite::params![],
+    )
+    .unwrap();
+
+    let uuid = format!("{}", uuid::Uuid::new_v4());
+    let name = path.into_inner();
+
+    // println!("{} {}", uuid, name);
+    conn.execute(
+        "INSERT INTO users (id, name) VALUES ($1, $2)",
+        rusqlite::params![&uuid, &name],
+    )
+    .unwrap();
+
+    let res = conn.query_row("SELECT name FROM users WHERE id=$1", &[&uuid], |row| {
+        row.get::<_, String>(0)
+    });
+
+    let res = res
+        .map(|user| HttpResponse::Ok().json(user))
+        .map_err(|_| HttpResponse::InternalServerError())?;
     Ok(res)
 }
 
@@ -45,7 +101,8 @@ async fn main() -> io::Result<()> {
         App::new()
             .data(pool.clone()) // <- store db pool in app state
             .wrap(middleware::Logger::default())
-            .route("/{name}", web::get().to(index))
+            //.route("/{name}", web::get().to(index))
+            .route("/{name2}", web::get().to(index2))
     })
     .bind("127.0.0.1:8080")?
     .run()
